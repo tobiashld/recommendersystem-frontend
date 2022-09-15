a643a3a0db9dfcd4ef3e31b5cc3ae900cf6914dc
tobiashld <tobias02173@gmail.com>
2022-09-15 02:24:16 +0200

    basic benutzeroberfläche

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [benutzerkennung, setBenutzerkennung] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-box">
+        <div>
+          <h3>Login</h3>
+        </div>
+      </div>
     </div>
   );
 }
