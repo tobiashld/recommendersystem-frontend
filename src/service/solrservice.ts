9ef60f60abfdd1f81ecbcf18c0efc252982f5e47
tobiashld <tobias02173@gmail.com>
2022-09-17 01:46:06 +0200

    fetch from real db

--- src/service/solrservice.ts
+++ src/service/solrservice.ts
@@ -2,19 +2,19 @@ function suchFilmeZuVolltext(suchString:string){
 
     suchString = suchString.split(" ").length > 1?'"'+suchString+'"~2':"*"+suchString+"*";
 
-    // return fetch("http://199.247.20.90:8983/solr/filme/select?debugQuery=false&indent=true&q.op=OR&q=volltextName%3A"+suchString+"",
-    //     {
-    //         method: "GET",
-    //         mode:'cors', 
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         }
-    //     })
-    //     .then(response=>response.json())
-    //     .then(responsejson=>
-    //         mapping(responsejson)
-    //         )
-    //     .catch(error=>{throw new Error(error)})
+    return fetch("https://solrrecommendersystem.cf:8984/solr/filme/select?debugQuery=false&indent=true&q.op=OR&q=volltextName%3A"+suchString+"",
+        {
+            method: "GET",
+            mode:'cors', 
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response=>response.json())
+        .then(responsejson=>
+            mapping(responsejson)
+            )
+        .catch(error=>{throw new Error(error)})
         
 
         let test = 
