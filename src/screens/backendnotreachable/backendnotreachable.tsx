7dabbea97bbf1cbb82bdfe390203f78fe4b96b4e
tobiashld <tobias02173@gmail.com>
2022-09-28 01:41:55 +0200

    backendnotreachable schöner gemacht

--- src/screens/backendnotreachable/backendnotreachable.tsx
+++ src/screens/backendnotreachable/backendnotreachable.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Loadingspinner from '../../components/loadingspinner/loadingspinner'
+import './backendnotreachable.css'
 
 function BackendNotReachable() {
   return (
-    <div className="App">
-        <div className="login-box">
+    <div className="App align-center">
+        <div className="login-box-small">
             <h3>Warten auf Backend</h3>
             <Loadingspinner size='Big' />
         </div>
