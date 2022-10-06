4bb4d011847c0bb804f179bb14b4d8a59e1ec0ea
tobiashld <tobias02173@gmail.com>
2022-10-06 11:22:23 +0200

    fixed dropdown spacing

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -26,7 +26,7 @@ function Dropdown(props:{items:FilmitemTypeBewertet[]|undefined,onItemClick:((it
       accessibility:true,
       dots:false,
       speed: 500,
-      slidesToShow: 4,
+      slidesToShow: 3,
       slidesToScroll: 3,
       initialSlide: 0,
       dragable:true,
