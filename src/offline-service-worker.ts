5dda2791c7863d44167eba523fe45b7f60b8d753
tobiashld <tobias02173@gmail.com>
2022-10-11 00:38:12 +0200

    wegen linter geändert

--- src/offline-service-worker.ts
+++ src/offline-service-worker.ts
@@ -1,3 +1,5 @@
-self.addEventListener('fetch',event=>{
-    console.log(event)
-})
+// self.addEventListener('fetch',event=>{
+//     console.log(event)
+// })
+const test = "teste"
+export default test
