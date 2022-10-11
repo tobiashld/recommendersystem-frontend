f9779531e76e79d2d6ea205817ae1089474cc44d
tobiashld <tobias02173@gmail.com>
2022-10-12 00:29:00 +0200

    eslints

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/error/store";
 import { register } from "./serviceWorkerRegistration";
@@ -22,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
+register();
