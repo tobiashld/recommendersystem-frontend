a38e27fb04be3ee47094a4e95f8fac5d7a1d6355
tobiashld <tobias02173@gmail.com>
2022-10-19 18:53:56 +0200

    cache forwarding

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -112,46 +112,49 @@ self.addEventListener('activate', event => {
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
-// self.addEventListener('fetch', event => {
-//   // Skip cross-origin requests, like those for Google Analytics.
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-//         return caches.open(RUNTIME).then(cache => {
-//           return fetch(event.request).then(response => {
-//             // Put a copy of the response in the runtime cache.
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
-self.addEventListener('fetch', (event) => {
-  event.respondWith(async function() {
-    // Try the cache
-    const cachedResponse = await caches.match(event.request);
-    if (cachedResponse) return cachedResponse;
-
-    try {
-      // Fall back to network
-      const response = await fetch(event.request)
-      console.log(event.request)
-      return response;
-    } catch (err) {
-      // If both fail, show a generic fallback:
-      var init = { "status" : 404 , "statusText" : "Sie sind weder online noch haben sie die Seite gecached" };
-    return new Response(null,init)
-      // However, in reality you'd have many different
-      // fallbacks, depending on URL & headers.
-      // Eg, a fallback silhouette image for avatars.
-    }
-    
-  }());
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }else{
+    event.respondWith(async function(){return await fetch(event.request)}())
+  }
 });
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(async function() {
+//     // Try the cache
+//     if(event.request.url === "")
+//     const cachedResponse = await caches.match(event.request);
+//     if (cachedResponse) return cachedResponse;
+
+//     try {
+//       // Fall back to network
+//       const response = await fetch(event.request)
+//       console.log(event.request)
+//       return response;
+//     } catch (err) {
+//       // If both fail, show a generic fallback:
+//       var init = { "status" : 404 , "statusText" : "Sie sind weder online noch haben sie die Seite gecached" };
+//     return new Response(null,init)
+//       // However, in reality you'd have many different
+//       // fallbacks, depending on URL & headers.
+//       // Eg, a fallback silhouette image for avatars.
+//     }
+    
+//   }());
+// });
